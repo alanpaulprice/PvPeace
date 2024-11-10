77bd592cdeda73d4b9c4bc4f8e3b1bb83ef6307d
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-11-10 13:44:39 +0000

    HideWinsFromUIWidgetTopCenter.lua: add logic to hide on initial load, add 'is shuffle' check

--- HideWinsFromUIWidgetTopCenter.lua
+++ HideWinsFromUIWidgetTopCenter.lua
@@ -1,10 +1,21 @@
 local _, addon = ...
 
 function addon:HideWinsFromUIWidgetTopCenter()
+	local winsTextFrame = UIWidgetTopCenterContainerFrame.widgetFrames[4457]
+
+	if winsTextFrame ~= nil and winsTextFrame:IsShown() then
+		winsTextFrame:Hide()
+	end
+
 	UIWidgetTopCenterContainerFrame:HookScript("OnEvent", function(self, event)
 		local winsTextFrame = self.widgetFrames[4457]
 
-		if event == "UPDATE_UI_WIDGET" and winsTextFrame ~= nil and winsTextFrame:IsShown() then
+		if
+			C_PvP.IsRatedSoloShuffle()
+			and event == "UPDATE_UI_WIDGET"
+			and winsTextFrame ~= nil
+			and winsTextFrame:IsShown()
+		then
 			winsTextFrame:Hide()
 		end
 	end)
